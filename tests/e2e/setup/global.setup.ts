2329007db25807bebd31ccf60c52c09b09af9816
Marius L. Jensen <468735+Clorith@users.noreply.github.com>
2026-03-08 15:16:03 +0100

    CI/CD: Improve the test runner to account for GitHub repo names, and simplify the text-environment setup

--- tests/e2e/setup/global.setup.ts
+++ tests/e2e/setup/global.setup.ts
@@ -5,11 +5,11 @@ setup( 'global setup', async() => {
 	// Run any pending WordPress database upgrades so wp-admin is accessible.
 	wpCli( 'core update-db' );
 
-	// Deactivate all additional plugins that are used as part of debugging and development.
-	wpCli( 'plugin deactivate --all' );
+	// Check if this is running via GitHub and has a different plugin slug for its CWD.
+	const repoName = process.env.GITHUB_REPOSITORY?.split( '/' ).pop() ?? 'fraktvalg';
 
 	// Activate WooCommerce and our plugin.
-	wpCli( 'plugin activate woocommerce fraktvalg' );
+	wpCli( 'plugin activate woocommerce ${ repoName }' );
 
 	// Prevent WordPress from showing the admin email verification interstitial
 	// during tests by setting the lifespan to 10 years from now.
