b6d3f2793e9d3c2173fb0b25252312b0abb39f09
Marius L. Jensen <468735+Clorith@users.noreply.github.com>
2026-03-08 15:22:40 +0100

    CI/CD: uickly enable all plugins in the test environment after previous cleanup

--- tests/e2e/setup/global.setup.ts
+++ tests/e2e/setup/global.setup.ts
@@ -5,11 +5,8 @@ setup( 'global setup', async() => {
 	// Run any pending WordPress database upgrades so wp-admin is accessible.
 	wpCli( 'core update-db' );
 
-	// Check if this is running via GitHub and has a different plugin slug for its CWD.
-	const repoName = process.env.GITHUB_REPOSITORY?.split( '/' ).pop() ?? 'fraktvalg';
-
 	// Activate WooCommerce and our plugin.
-	wpCli( 'plugin activate woocommerce ${ repoName }' );
+	wpCli( 'plugin activate --all' );
 
 	// Prevent WordPress from showing the admin email verification interstitial
 	// during tests by setting the lifespan to 10 years from now.
